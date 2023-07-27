31e7aaa0a8782e8dc0953b21400e22c7badda0b8
TBHLLL <2392730043@qq.com>
2023-07-27 18:50:02 +0800

    Revert "all_up update"
    This reverts commit 4033a8a55142095d589e02fc59df56bb84392acb.

--- article/views.py
+++ article/views.py
@@ -9,6 +9,8 @@ import markdown
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
+from comment.models import Comment
+from comment.forms import CommentForm
 
 
 def article_list(request):
